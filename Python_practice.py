59be5ceb723efcbac15d8c752dc5065e9a3057e1
oybekg <65194752+oybekg@users.noreply.github.com>
2020-08-07 14:37:14 -0400

    Delete Python_practice.py

--- Python_practice.py
+++
@@ -1,8 +0,0 @@
-counties_dict = {"Arapahoe": 422829, "Denver": 463353, "Jefferson": 432438}
-voting_data = [{"county":"Arapahoe", "registered_voters": 422829},
-                {"county":"Denver", "registered_voters": 463353},
-                {"county":"Jefferson", "registered_voters": 432438}]
-
-for county_dict in voting_data:
-    for value in county_dict.values():
-        print(value)
